0358c9805c78b997c8fd7100c481831834cca2fe
Ant <antony@springtimesoft.co.nz>
2019-08-21 19:05:25 +1200

    Make the code a little more robust

--- URLBuilder.php
+++ URLBuilder.php
@@ -13,33 +13,46 @@ class URLBuilder
 
   public function __construct($params) {
 
-    if (is_array($params) && isset($params['host'])) {
+    // Query index exists but isn't an array, so throw exception
+    if(array_key_exists('query', $params) && !is_array($params['query'])) {
 
-        $this->scheme = isset($params['scheme']) ? $params['scheme'] : 'http';
-        $this->user = isset($params['user']) ? $params['user'] : '';
-        $this->pass = isset($params['pass']) ? ':' . $params['pass'] : '';
-        $this->host = $params['host'];
-        $this->port = isset($params['port']) ? $params['port'] : 80;
-        $this->path = isset($params['path']) ? $params['path'] : '/';
-        $this->query = isset($params['query']) ? $params['query'] : [];
-        $this->fragment = isset($params['fragment']) ? '#' . $params['fragment'] : '';
+      throw new Exception('Sorry, if query index is specified it needs to be an array');
+
+    }
+
+    // Host is specified so set the default values
+    if ((isset($params['host']) && $params['host'] != null)) {
+
+        $this->host =     $params['host'];
+        $this->scheme =   (isset($params['scheme']) && $params['scheme'] != null) ? $params['scheme'] : 'http';
+        $this->user =     (isset($params['user']) && $params['user'] != null) ? $params['user'] : null;
+        $this->pass =     (isset($params['pass']) && $params['pass'] != null) ? $params['pass'] : null;
+        $this->port =     (isset($params['port']) && $params['port'] != null) ? $params['port'] : 80;
+        $this->path =     (isset($params['path']) && $params['path'] != null) ? $params['path'] : '/';
+        $this->fragment = (isset($params['fragment']) && $params['fragment'] != '') ? '#' . $params['fragment'] : null;
+        $this->query =    (isset($params['query']) && $params['query'] != null) ? $params['query'] : [];
 
     } else {
 
-        throw new Exception('Some fields not formatted correctly');
+      throw new Exception('Sorry, host is a required index and needs to be specified');
+
     }
 
   }
 
+  //
+  // Return the built url in a string
+  //
   function build() {
 
-    $pass = ($this->user || $this->pass) ? $this->pass . '@' : '';
+    // If username, password or port aren't specified, they must not be shown
+    $userInfo = ($this->user && $this->pass) ? $this->user .':'. $this->pass . '@' : '';
+    $port = ($this->port == 80 || $this->port == '') ? '' : ':'.$this->port;
 
     $constructed_url =  (string)  $this->scheme .'://' .
-                                  $this->user .
-                                  $pass .
+                                  $userInfo .
                                   $this->host .
-                                  ':' . $this->port .
+                                  $port .
                                   $this->path .
                                   implode($this->query) .
                                   $this->fragment;
@@ -51,10 +64,15 @@ class URLBuilder
 
 $params = array(
                   'host' => 'blah.com',
-
+                  'user' => 'ant',
+                  'pass' => 'admin',
+                  'port' => 8080,
+                  'query' => ['?arg=val'],
+                  'path' => '/about',
+                  'fragment' => 'anchor'
           );
 
 $built_url = new URLBuilder($params);
 
-var_dump($built_url->build());
+echo $built_url->build();
 
